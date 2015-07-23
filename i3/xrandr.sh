ba3e86f02f0411ab627940750e321fa665dfd81b
Hendrik Bunke <h.bunke@zbw.eu>
2015-07-23 17:45:00 +0200

    moved xrandr to ~/bin

--- i3/xrandr.sh
+++ i3/xrandr.sh
@@ -1,2 +1,1 @@
-#!/bin/sh
-xrandr --output VIRTUAL1 --off --output DP1 --off --output HDMI3 --mode 1920x1200 --pos 1920x0 --rotate normal --output HDMI2 --off --output HDMI1 --mode 1920x1200 --pos 0x0 --rotate normal --output VGA1 --off
+/home/bunke/bin/xrandr.sh
